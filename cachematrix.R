3bb224cb94b0f1193471acdb665e679456d552f8
Heena <Heena@dominodatalab.com>
2016-08-03 15:24:31 -0700

    updated files
    saving changes

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,49 @@
 ## Write a short comment describing this function
 ##writing a new comment
 
+## makeCacheMatrix creates and returns a list of functions
+## used by cacheSolve to get or set the inverted matrix in cache
 makeCacheMatrix <- function(x = matrix()) {
-
+  # stores the cached value
+  # initialize to NULL
+  cache <- NULL
+  
+  # create the matrix in the working environment
+  set <- function(y) {
+    x <<- y
+    cache <<- NULL
+  }
+  
+  # get the value of the matrix
+  get <- function() x
+  # invert the matrix and store in cache
+  setMatrix <- function(inverse) cache <<- inverse
+  # get the inverted matrix from cache
+  getInverse <- function() cache
+  
+  # return the created functions to the working environment
+  list(set = set, get = get,
+       setMatrix = setMatrix,
+       getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## attempt to get the inverse of the matrix stored in cache
+  cache <- x$getInverse()
+  
+  # return inverted matrix from cache if it exists
+  # else create the matrix in working environment
+  if (!is.null(cache)) {
+    message("getting cached data")
+    
+    # display matrix in console
+    return(cache)
+  }
+  matrix <- x$get()
+  cache <- solve(matrix, ...)
+ 
+  return (cache)
 }
